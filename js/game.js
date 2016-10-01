594c011047505db5fabf0f3c66cd5ed2b21d932c
Vladislav Gasanov <kosmoflyko@gmail.com>
2016-10-01 14:14:56 +0300

    fix move

--- js/game.js
+++ js/game.js
@@ -10,6 +10,13 @@ var game = new Phaser.Game(
     }
 );
 
+var player;
+var cursors;
+var platforms;
+var stars;
+var score = 0;
+var scoreText;
+
 function preload() {
     game.load.image('sky', 'assets/sky.png');
     game.load.image('ground', 'assets/platform.png');
@@ -35,6 +42,8 @@ function create() {
     ledge = platforms.create(-150, 250, 'platform');
     ledge.body.immovable = true;
 
+    scoreText = game.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#A00' });
+
     player = game.add.sprite(32, game.world.height - 150, 'dude');
     game.physics.arcade.enable(player);
 
@@ -46,32 +55,46 @@ function create() {
     player.animations.add('right', [5, 6, 7, 8], 10, true);
 
     cursors = game.input.keyboard.createCursorKeys();
+
+    stars = game.add.group();
+    stars.enableBody = true;
+
+    for (var i = 0; i < 12; i++){
+        var star = stars.create(i * 70, 0, 'star');
+        star.body.gravity.y = 6;
+        star.body.bounce.y = 0.7 + Math.random() * 0.2;
+    }
 }
 
 function update() {
     game.physics.arcade.collide(player, platforms);
+    game.physics.arcade.collide(stars, platforms);
+    game.physics.arcade.overlap(player, stars, collectStars, null, this);
 
     player.body.velocity.x = 0;
 
-    if (cursors.left.isDown)
-    {
+    if (cursors.left.isDown) {
         player.body.velocity.x = -150;
         player.animations.play('left');
     }
-    else if (cursors.right.isDown)
-    {
+    else if (cursors.right.isDown) {
         player.body.velocity.x = 150;
 
         player.animations.play('right');
     }
-    else
-    {
+    else {
         player.animations.stop();
         player.frame = 4;
     }
 
-    if (cursors.up.isDown && player.body.touching.down)
-    {
+    if (cursors.up.isDown && player.body.touching.down) {
         player.body.velocity.y = -350;
     }
+}
+
+function collectStars(player, star) {
+    star.kill();
+
+    score += 10;
+    scoreText.text = "Score: " + score;
 }
