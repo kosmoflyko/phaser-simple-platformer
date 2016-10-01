a783eeb2855a23a38f3046df0d187d379319a7e1
Vladislav Gasanov <kosmoflyko@gmail.com>
2016-10-01 13:28:34 +0300

    add move

--- js/game.js
+++ js/game.js
@@ -45,8 +45,26 @@ function create() {
     player.animations.add('left', [0, 1, 2, 3], 10, true);
     player.animations.add('right', [5, 6, 7, 8], 10, true);
 
+    cursors = game.input.keyboard.createCursorKeys()
 }
 
 function update() {
     game.physics.arcade.collide(player, platforms);
+
+    player.body.velocity.x = 0;
+
+    if (cursors.left.isDown){
+        player.body.velocity.x = -150;
+        player.animations.play('left');
+    } else if (cursors.right.isDown) {
+        player.body.velocity.x = 150;
+        player.animations.play('right');
+    } else {
+        player.animations.stop();
+        player.setFrame = 4;
+    }
+
+    if (cursors.up.isDown && player.body.touching.down) {
+        player.body.velocity.y = -350;
+    }
 }
