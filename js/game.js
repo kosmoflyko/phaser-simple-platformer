74afdb8bbbdac39f0a37eae10b8a2d72b19e77a3
Vladislav Gasanov <kosmoflyko@gmail.com>
2016-10-01 13:09:17 +0300

    add platforms and player

--- js/game.js
+++ js/game.js
@@ -1,3 +1,53 @@
 var game = new Phaser.Game(
-
+    800,
+    600,
+    Phaser.AUTO,
+    '#game',
+    {
+        preload: preload,
+        create: create,
+        update: update
+    }
 );
+
+function preload() {
+    game.load.image('sky', 'assets/sky.png');
+    game.load.image('ground', 'assets/platform.png');
+    game.load.image('platform', 'assets/sky-platform.png');
+    game.load.image('star', 'assets/star.png');
+    game.load.spritesheet('dude', 'assets/dude.png', 32, 48)
+}
+
+function create() {
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    game.add.sprite(0,0,'sky');
+
+    platforms = game.add.group();
+    platforms.enableBody = true;
+
+    var ground = platforms.create(0 , game.world.height - 64, 'ground');
+    ground.scale.setTo(2,2);
+    ground.body.immuvable = true;
+    ground.body.moves = false;
+
+    var ledge = platforms.create(400, 400, 'platform');
+    ledge.immuvable = true;
+    ledge.body.moves = false;
+
+    ledge = platforms.create(-150, 250, 'platform');
+    ledge.immuvable = true;
+    ledge.body.moves = false;
+
+    player = game.add.sprite(32, game.world.height - 150, 'dude');
+    game.physics.arcade.enable(player);
+    player.body.bounce.y = 0.2;
+    player.body.gravity.y = 300;
+    player.body.collideWorldBounds = true;
+    player.animations.left('left', [0, 1, 2, 3], 10, true);
+    player.animations.right('right', [5, 6, 7, 8], 10, true);
+
+}
+
+function update() {
+    game.physics.arcade.collide(player, platforms);
+}
